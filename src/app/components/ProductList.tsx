ae0098fdab661351abf0fa91d2c69eb778f09ddf
luanamsilva <luanamoura2002@yahoo.com.br>
2023-07-31 08:29:38 -0300

    component-Product-List

---
+++ src/app/components/ProductList.tsx
@@ -0,0 +1,22 @@
+import { Item } from './../components/Item';
+import { itemsData } from '../data/items';
+import Link from 'next/link';
+
+export default function ProductList() {
+  return (
+    <div className='flex flex-wrap'>
+      {itemsData.map((item) => (
+        <Link key={item.id} href={`/products/${item.id}`} passHref>
+          
+            <Item
+              key={item.id}
+              title={item.title}
+              theme={item.theme}
+              image={item.image}
+            />
+          
+        </Link>
+      ))}
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,12 @@
-import { Item } from './components/Item';
-import { itemsData } from './data/items';
+import  ProductList  from './components/ProductList';
+
 
 
 export default function Home() {
 
   return (
     <>
-    
-  <div className='flex flex-wrap'>
-      {itemsData.map((item) => (
-        
-        <Item
-        key={item.id} 
-        title={item.title} 
-        theme={item.theme} 
-        image={item.image}
-    
-        />
-      ))}
-      </div>
+  <ProductList/>
 
     </>
   );
