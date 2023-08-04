e6e46204d5fd7d10ea9ace8bdbabef397b97e5ef
luanamsilva <luanamoura2002@yahoo.com.br>
2023-08-04 08:38:10 -0300

    nav-responsive

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,10 +1,15 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { navLinks } from '../data/nav';
 
+
 export const Header = () => {
+  const [showMenu, setShowMenu] = useState(false)
+  const handleShowMenu = () =>{
+    setShowMenu(!showMenu)
+  }
   return (
     <header>
       <div className="flex justify-between flex-wrap sm:w-full">
@@ -20,11 +25,10 @@ export const Header = () => {
           </Link>
         </div>
 
-        <div className="flex justify-center items-center mt-4 sm:mt-0 w-full sm:w-auto mb-4">
-  
-        </div>
+
+        <div className="flex justify-center items-center mt-4 sm:mt-0 w-full sm:w-auto mb-4"></div>
       </div>
-      <nav className="bg-main">
+      <nav className={`bg-main ${showMenu ? 'block' : 'hidden'} md:block`}>
         <ul className="flex text-white text-md flex-col items-center sm:flex-row sm:justify-center sm:items-center p-3 gap-10 ">
           <li className="text-white text-md hover:text-gray-200">
             <Link href="/">Página Inicial</Link>
@@ -36,6 +40,31 @@ export const Header = () => {
           ))}
         </ul>
       </nav>
+      <div className="bg-main flex justify-end  md:hidden">
+        <button
+          id="menuButton"
+          type="button"
+          className="inline-flex items-center justify-center rounded-md p-1 pr-4 text-rose hover:text-gray-300 transition duration-150 ease-in-out"
+          aria-label="Menu"
+          aria-expanded="false"
+          onClick={handleShowMenu}
+        >
+          <svg
+            className="h-6 w-6"
+            stroke="currentColor"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h15M4 12h16M4 18h16"
+            ></path>
+          </svg>
+        </button>
+      
+      </div>
     </header>
   );
 };
